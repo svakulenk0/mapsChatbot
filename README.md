7f699cc75ce92c38d7f13929139354c8539b2be3
vendi <svitlana.vakulenko@gmail.com>
2018-07-24 10:34:31 +0200

    new: google maps library setup

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Chatbot that helps you to find routes through Google API
 
+(https://github.com/googlemaps/google-maps-services-python)
+
 
 ## Similar projects
 
 * [MapBot](https://github.com/vishakha-lall/MapBot)
-
-* [err-geoloc](https://github.com/USDevOps/err-geoloc)
--- maps_connector.py
+++ maps_connector.py
@@ -7,12 +7,34 @@ Created on Jul 24, 2018
 .. codeauthor: svitlana vakulenko
     <svitlana.vakulenko@gmail.com>
 
-Connects to Google Maps API
-https://cloud.google.com/maps-platform/routes/
-https://developers.google.com/maps/documentation/directions/start
+Connects to Google Maps API via the offcial Python library
+
+https://github.com/googlemaps/google-maps-services-python
 
 '''
-# URL = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Victoria+BC&key=%s"
-URL = "https://maps.googleapis.com/maps/api/directions/json?origin=%s&destination=%s&key=%s"
+import googlemaps
+from datetime import datetime
 
-def get_route(origin='WU Wien', destination='Zoo Schoenbrunn'):
+from settings import API_KEY
+
+
+# connect to Google Maps API
+gmaps = googlemaps.Client(key=API_KEY)
+
+
+def get_route(origin, destination):
+    # Request directions via public transit
+    now = datetime.now()
+    directions_result = gmaps.directions("Sydney Town Hall",
+                                         "Parramatta, NSW",
+                                         mode="transit",
+                                         departure_time=now)
+    return directions_result
+
+
+def test_get_route(origin='WU Wien', destination='Zoo Schoenbrunn'):
+    print get_route(origin, destination)
+
+
+if __name__ == '__main__':
+    test_get_route()
