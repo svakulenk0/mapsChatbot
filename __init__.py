75781a9e363509c23e08f67b13e59dd61525bbfc
vendi <svitlana.vakulenko@gmail.com>
2018-07-25 09:49:18 +0200

    fix

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-from opsdroid.matchers import match_regex
+from opsdroid.matchers import match_always
 import logging
 import random
 
 from .maps_connector import rank_alternative_routes
 
 
-# @match_regex(r'from (.*) to (.*)', "i")
-@match_regex(r'from (.*) to (.*)')
+@match_always()
+@match_regex(r'from (.*) to (.*)', case_sensitive=False)
 async def start(opsdroid, config, message):
     origin = message.regex.group(1)
     destination = message.regex.group(2)
